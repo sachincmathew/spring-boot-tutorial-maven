9f7d6401eb76f384888fb919a80f1e45f91bc1b2
sachincmathew <sachincmathew@gmail.com>
2023-12-12 19:14:11 +0800

    first commit

---
+++ src/test/java/org/example/springboottutorialmaven/SpringBootTutorialMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springboottutorialmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutorialMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
