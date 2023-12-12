d0f576a22189dc2473678eccf9d47c43c2fa0b7d
sachincmathew <sachincmathew@gmail.com>
2023-12-12 19:37:31 +0800

    Customer add,findByID,findAll added

---
+++ src/main/java/org/example/springboottutorialmaven/CustomerRepository.java
@@ -0,0 +1,8 @@
+package org.example.springboottutorialmaven;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Integer> {
+
+    Customer findCustomerById(Integer id);
+}
